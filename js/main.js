7989f9a04e6d3e0f251726cb424837eb5b2efc20
urbanoluigi <urrbanoluigi@gmail.com>
2023-11-22 14:13:39 +0100

    array creata

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//Crea un array composto da 10 automobili.
+
+let automobili = [
+    {marca:"toyota", modello:"prius", alimentazione:"elettrica" },
+    {marca:"ford", modello:"mustang", alimentazione:"gpl" },
+    {marca:"honda", modello:"accord", alimentazione:"diesel" },
+    {marca:"volkswagen", modello:"golf", alimentazione:"ibrida" },
+    {marca:"chevrolet", modello:"malibu", alimentazione:"benzina" },
+    {marca:"nissan", modello:"rogue", alimentazione:"gpl" },
+    {marca:"bmw", modello:"3 series", alimentazione:"ibrida" },
+    {marca:"mercedes-benz", modello:"c-class", alimentazione:"diesel" },
+    {marca:"audi", modello:"a4", alimentazione:"benzina" },
+    {marca:"tesla", modello:"model3", alimentazione:"elettrica" },
+
+]
